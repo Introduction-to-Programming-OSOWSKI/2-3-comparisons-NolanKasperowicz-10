b549feed548b445c90deb38fc2a85a04cba9f904
NolanKasperowicz-10 <831223@mystma.org>
2022-09-29 13:56:17 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        print("true")
+    else:
+        print("false")
+(greaterThan(6, 10))
+
+def lessThan(x, y):
+    if x == y:
+        print("true")
+    else:
+        print("false")
+(lessThan(23, 23))
+
+def lessOrEqual(x, y):
+    if x <= y:
+        print("true")
+    else:
+        print("false")
+(lessOrEqual(99, 100))
+
+def lessOrEqual(x, y):
+    if x <= y:
+        print("true")
+    else:
+        print("false")
+(lessOrEqual(100, 100))
+
+def lessOrEqual(x, y):
+    if x <= y:
+        print("true")
+    else:
+        print("false")
+(lessOrEqual(101, 100))
